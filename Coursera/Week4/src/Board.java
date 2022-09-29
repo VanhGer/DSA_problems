d126d84c4bbad545f8b31597edf6ffaa616d5938
Phạm Gia Việt Anh <vietanhpg2003@gmail.com>
2022-09-29 13:13:17 +0700

    Week4-Coursera

---
+++ Coursera/Week4/src/Board.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Board {
+
+  private int[][] a;
+  private int sz;
+  // create a board from an n-by-n array of tiles,
+  // where tiles[row][col] = tile at (row, col)
+  public Board(int[][] tiles) {
+    a = new int[tiles.length][];
+    sz = tiles.length;
+    for (int i = 0; i < tiles.length; i++) {
+      a[i] = tiles[i].clone();
+    }
+  }
+
+  // string representation of this board
+  public String toString() {
+    String res = sz + "\n";
+    for (int i = 0; i < sz; i++) {
+      res += " ";
+      for (int j = 0; j < sz; j++) res += a[i][j] + " ";
+      res += "\n";
+    }
+    return res;
+  }
+
+  // board dimension n
+  public int dimension() {
+    return sz;
+  }
+
+  // number of tiles out of place
+  public int hamming() {
+    int res = 0;
+    for (int i = 0; i < sz; i++) {
+      for (int j = 0; j < sz; j++) {
+        if (a[i][j] == 0) continue;
+        if (a[i][j] != (i * sz + j + 1)) {
+          res++;
+        }
+      }
+    }
+    return res;
+  }
+
+  // sum of Manhattan distances between tiles and goal
+  public int manhattan() {
+    int res = 0;
+    for (int i = 0; i < sz; i++)
+      for (int j = 0; j < sz; j++) {
+        if (a[i][j] == 0) continue;
+        int x = (a[i][j] - 1) / sz;
+        int y = (a[i][j] - 1) % sz;
+        res += Math.abs(x - i) + Math.abs(y - j);
+      }
+    return res;
+  }
+
+  // is this board the goal board?
+  public boolean isGoal() {
+    return manhattan() == 0;
+  }
+
+  // does this board equal y?
+  public boolean equals(Object y) {
+    if (y instanceof Board) {
+      if (((Board) y).dimension() != this.dimension()) return false;
+      return Arrays.deepEquals(this.a, ((Board) y).a);
+    }
+    else return false;
+  }
+
+  private void exch(int x1, int y1, int x2, int y2) {
+    int tmp = a[x1][y1];
+    a[x1][y1] = a[x2][y2];
+    a[x2][y2] = tmp;
+  }
+  // all neighboring boards
+  public Iterable<Board> neighbors() {
+    List<Board> boardList = new ArrayList<>();
+    int x = -1, y = -1;
+    boolean ok = false;
+    for (int i = 0; i < sz; i++) {
+      for (int j = 0; j < sz; j++) {
+        if (a[i][j] == 0) {
+          x = i;
+          y = j;
+          ok = true;
+          break;
+        }
+      }
+      if (ok) break;
+    }
+    if (0 <= x + 1 && x + 1 < sz) {
+      Board neighbor = new Board(this.a);
+      neighbor.exch(x, y, x + 1, y);
+      boardList.add(neighbor);
+    }
+    if (0 <= x - 1 && x - 1 < sz) {
+      Board neighbor = new Board(this.a);
+      neighbor.exch(x, y, x - 1, y);
+      boardList.add(neighbor);
+    }
+    if (0 <= y + 1 && y + 1 < sz) {
+      Board neighbor = new Board(this.a);
+      neighbor.exch(x, y, x, y + 1);
+      boardList.add(neighbor);
+    }
+    if (0 <= y - 1 && y - 1 < sz) {
+      Board neighbor = new Board(this.a);
+      neighbor.exch(x, y, x, y - 1);
+      boardList.add(neighbor);
+    }
+    return boardList;
+  }
+
+  // a board that is obtained by exchanging any pair of tiles
+  public Board twin() {
+    Board twin = new Board(this.a);
+    if (sz == 1) return twin;
+    if (twin.a[0][0] != 0 && twin.a[0][1] != 0)
+      twin.exch(0, 0, 0, 1);
+    else twin.exch(sz - 1, sz - 2, sz - 1, sz - 1);
+    return twin;
+  }
+
+  // unit testing (not graded)
+}
---
+++ Coursera/Week4/src/Solver.java
@@ -0,0 +1,118 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solver {
+
+  private boolean solvable;
+  private int moves;
+  private Node goal;
+
+  // find a solution to the initial board (using the A* algorithm)
+  public Solver(Board initial) {
+    if (initial == null) throw new IllegalArgumentException();
+    MinPQ<Node> q = new MinPQ<>();
+    q.insert(new Node(null, 0, initial, true));
+    q.insert(new Node(null, 0, initial.twin(), false));
+    Node u = null;
+    while (!q.isEmpty()) {
+      u = q.delMin();
+      if (u.board.isGoal()) break;
+      for (Board x : u.board.neighbors()) {
+        if (u.preBoard == null || !x.equals(u.preBoard.board)) {
+          q.insert(new Node(u, x));
+        }
+      }
+    }
+    if (!u.state || !u.board.isGoal()) {
+      solvable = false;
+      moves = -1;
+      goal = null;
+    } else {
+      goal = u;
+      solvable = true;
+      moves = goal.moves;
+    }
+  }
+
+  private class Node implements Comparable<Node> {
+    private Node preBoard;
+    private int moves;
+    private int priority;
+    private Board board;
+    private boolean state;
+
+    public Node(Node preBoard, int moves, Board board, boolean state) {
+      this.preBoard = preBoard;
+      this.moves = moves;
+      this.board = board;
+      this.state = state;
+      this.priority = this.board.manhattan() + this.moves;
+    }
+
+    public Node(Node preBoard, Board board) {
+      this.preBoard = preBoard;
+      this.board = board;
+      this.moves = preBoard.moves + 1;
+      this.state = preBoard.state;
+      this.priority = this.board.manhattan() + this.moves;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+      if (this.priority != o.priority) {
+        return this.priority - o.priority;
+      }
+      return this.board.manhattan() - o.board.manhattan();
+    }
+  }
+
+  // is the initial board solvable? (see below)
+  public boolean isSolvable() {
+    return this.solvable;
+  }
+
+  // min number of moves to solve initial board; -1 if unsolvable
+  public int moves() {
+    return this.moves;
+  }
+
+  // sequence of boards in a shortest solution; null if unsolvable
+  public Iterable<Board> solution() {
+    if (goal == null) {
+      return null;
+    }
+    Node cur = goal;
+    Stack<Board> res = new Stack<>();
+    while (cur != null) {
+      res.push(cur.board);
+      cur = cur.preBoard;
+    }
+    return res;
+  }
+
+  // test client (see below)
+  public static void main(String[] args) {
+
+    // create initial board from file
+    In in = new In("puzzle3x3-unsolvable.txt");
+    int n = in.readInt();
+    int[][] tiles = new int[n][n];
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) tiles[i][j] = in.readInt();
+    Board initial = new Board(tiles);
+
+    // solve the puzzle
+    Solver solver = new Solver(initial);
+
+    // print solution to standard output
+    if (!solver.isSolvable()) StdOut.println("No solution possible");
+    else {
+      StdOut.println("Minimum number of moves = " + solver.moves());
+      for (Board board : solver.solution()) StdOut.println(board);
+    }
+  }
+}
